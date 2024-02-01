f3f93d8788cbc741f6ab2c215b176233173b9ba6
Jaspreet Singh <115124290+jaz404@users.noreply.github.com>
2024-02-01 13:37:54 -0700

    Fixed the head

--- app/src/main/java/com/example/cmput301w24t22/Shape.java
+++ app/src/main/java/com/example/cmput301w24t22/Shape.java
@@ -3,11 +3,8 @@ package com.example.cmput301w24t22;
 public class Shape {
     public int x;
     public int y;
-<<<<<<< HEAD
+
     String color = "pink";
 
-=======
-    String color = "pink";
->>>>>>> 4667bb783a6d020d77e2d68243b2271ef48bc5f2
 
 }
